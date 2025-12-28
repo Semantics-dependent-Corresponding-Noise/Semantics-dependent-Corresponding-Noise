051cf3434cff5580b7a12ac3f1e9b21a693fb642
Semantic-dependent-Corresponding-Noise <251553252+Semantic-dependent-Corresponding-Noise@users.noreply.github.com>
2025-12-28 11:09:08 +0800

    Update f30k_object_omission_img5txt_noise.py

--- noise_construct/f30k_noise_construct/Object_Omission/f30k_object_omission_img5txt_noise.py
+++ noise_construct/f30k_noise_construct/Object_Omission/f30k_object_omission_img5txt_noise.py
@@ -22,7 +22,7 @@ for row in train_data.itertuples():
         current_index += 1
 
 # Path to the noise file
-noise_file_path = '/path/dataset/core_missing_Error_noise_5error_f30k/annotations/scan_split/1.0_noise_train_caps.txt'
+noise_file_path = '/path/dataset/Object_Omission_noise_5error_f30k/annotations/scan_split/1.0_noise_train_caps.txt'
 
 with open(noise_file_path, 'r', encoding='utf-8') as f:
     noise_texts = f.readlines()
@@ -53,7 +53,7 @@ for img_id in tqdm(images_to_replace, desc="Replacing images"):
 print(f"Total text descriptions replaced: {replacement_count}")
 
 # Output file path
-output_file_path = f'/path/dataset/core_missing_Error_noise5error_f30k/annotations/scan_split/{replace_ratio}_noise_train_caps.txt'
+output_file_path = f'/path/dataset/Object_Omission_noise_5error_f30k/annotations/scan_split/{replace_ratio}_noise_train_caps.txt'
 
 with open(output_file_path, 'w', encoding='utf-8') as f:
     for text in raw_texts:
