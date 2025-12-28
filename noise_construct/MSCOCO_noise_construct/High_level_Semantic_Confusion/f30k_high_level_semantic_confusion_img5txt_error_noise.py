df839c4371e8558c85cb7901224460856d07ffdf
Semantic-dependent-Corresponding-Noise <251553252+Semantic-dependent-Corresponding-Noise@users.noreply.github.com>
2025-12-28 10:28:38 +0800

    Update f30k_high_level_semantic_confusion_img5txt_error_noise.py

--- noise_construct/MSCOCO_noise_construct/High_level_Semantic_Confusion/f30k_high_level_semantic_confusion_img5txt_error_noise.py
+++ noise_construct/MSCOCO_noise_construct/High_level_Semantic_Confusion/f30k_high_level_semantic_confusion_img5txt_error_noise.py
@@ -2,14 +2,14 @@ import random
 from tqdm import tqdm
 
 # Read original text data
-file_path = '/path/dataset/k_means_5error_MSCOCO/annotations/scan_split/0_noise_train_caps.txt'
+file_path = '/path/dataset/High_level_Semantic_Confusion_5error_MSCOCO/annotations/scan_split/0_noise_train_caps.txt'
 
 with open(file_path, 'r', encoding='utf-8') as f:
     raw_texts = f.readlines()
 raw_texts = [text.strip() for text in raw_texts]
 
 # Noise file path
-noise_file_path = '/path/dataset/k_means_5error_MSCOCO/annotations/scan_split/1.0_noise_train_caps.txt'
+noise_file_path = '/path/dataset/High_level_Semantic_Confusion_5error_MSCOCO/annotations/scan_split/1.0_noise_train_caps.txt'
 
 with open(noise_file_path, 'r', encoding='utf-8') as f:
     noise_texts = f.readlines()
@@ -54,7 +54,7 @@ print(f"Total text descriptions replaced: {replacement_count}")
 print(f"Involving {num_images_to_replace} images")
 
 # Output file path
-output_file_path = f'/path/dataset/k_means_5error_MSCOCO/annotations/scan_split/{replace_ratio}_noise_train_caps.txt'
+output_file_path = f'/path/dataset/High_level_Semantic_Confusion_5error_MSCOCO/annotations/scan_split/{replace_ratio}_noise_train_caps.txt'
 
 with open(output_file_path, 'w', encoding='utf-8') as f:
     for text in raw_texts:
